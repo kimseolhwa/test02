adafc92270e4fa3a25830591e7dfc7ff65c63c76
kimyk0120 <kimyk0120@hotmail.com>
2016-02-26 17:16:18 +0900

    김영광 테스트01

--- gomgom-test/src/main/java/bitcamp/java77/Hello.java
+++ gomgom-test/src/main/java/bitcamp/java77/Hello.java
@@ -5,6 +5,16 @@ public class Hello {
 		System.out.println("gomgom test");
 		System.out.println("test");
 		System.out.println("dddd");
+
 		System.out.println("연주커밋");
+		
+		for(int i =1 ; i<5 ;i++){
+			System.out.println("김영광 커밋테스트");
+		}
+		
+		
+		
+
+
 	}
 }
